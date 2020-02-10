302c6bd88627e2922a34bbfdbfc4bb88d7cbf7f6
WMXPY <wm@mengw.io>
2020-02-10 10:25:48 -0600

    initial run

---
+++ example/lib/entry.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on'),
+        ),
+      ),
+    );
+  }
+}
