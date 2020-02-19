c306ff07a36b2be8db1905becf3b35a6a9e986ab
WMXPY <wm@mengw.io>
2020-02-19 10:11:26 -0600

    implement gather function

--- lib/src/auth/device.dart
+++ lib/src/auth/device.dart
@@ -1,3 +1,56 @@
-device_info: ^0.4.1+4
+import 'dart:io';
 
-Future<String> getDeviceInfo() async {}
+import 'package:device_info/device_info.dart';
+
+class DeviceIdentityInfo {
+  static Future<DeviceIdentityInfo> gather() async {
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+    try {
+      if (Platform.isAndroid) {
+        AndroidDeviceInfo info = await deviceInfo.androidInfo;
+
+        return DeviceIdentityInfo(
+          deviceName: info.model,
+          deviceModel: info.model,
+          deviceSystem: info.version.baseOS,
+          deviceVersion: info.version.toString(),
+          deviceIndetifier: info.androidId,
+          isPhysicalDevice: info.isPhysicalDevice,
+        );
+      } else if (Platform.isIOS) {
+        IosDeviceInfo info = await deviceInfo.iosInfo;
+
+        return DeviceIdentityInfo(
+          deviceName: info.name,
+          deviceModel: info.model,
+          deviceSystem: info.systemName,
+          deviceVersion: info.systemVersion,
+          deviceIndetifier: info.identifierForVendor,
+          isPhysicalDevice: info.isPhysicalDevice,
+        );
+      }
+    } on Exception {
+      return null;
+    }
+
+    return null;
+  }
+
+  final String deviceName;
+  final String deviceModel;
+  final String deviceSystem;
+  final String deviceVersion;
+  final String deviceIndetifier;
+
+  final bool isPhysicalDevice;
+
+  DeviceIdentityInfo({
+    this.deviceName,
+    this.deviceModel,
+    this.deviceSystem,
+    this.deviceVersion,
+    this.deviceIndetifier,
+    this.isPhysicalDevice,
+  });
+}
